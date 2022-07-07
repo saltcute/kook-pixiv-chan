284837bd1dda55b4cd4b66443ed11596bea4f702
potatopotat0 <xuanjiap@lolicon.ac.cn>
2022-07-08 03:44:26 +0800

    update

--- src/commands/pixiv/common/index.ts
+++ src/commands/pixiv/common/index.ts
@@ -3,7 +3,7 @@ export * from './linkmap'
 import { tagBanList } from './tagBanList';
 
 export namespace common {
-    export const akarin = "https://img.kaiheila.cn/assets/2022-07/vlOSxPNReJ0dw0dw.jpg";
+    export const akarin = "https://img.kookapp.cn/assets/2022-07/vlOSxPNReJ0dw0dw.jpg";
 
     export function isForbittedTag(tag: string) {
         if (tagBanList.includes(tag)) {
