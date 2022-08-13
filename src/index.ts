b9b3d1dacdcc023b6a9ec5fee3780f2298dadd9a
potatopotat0 <xuanjiap@lolicon.ac.cn>
2022-08-13 16:19:37 +0800

    Add experimental Chinese alias for commands

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ import auth from 'configs/auth';
 import config from './configs/config';
 import schedule from 'node-schedule';
 import { random } from 'commands/pixiv/pixiv.random.app';
+import { top } from 'commands/pixiv/pixiv.top.app';
+import { detail } from 'commands/pixiv/pixiv.detail.app';
+import { author } from 'commands/pixiv/pixiv.author.app';
 
 bot.logger.fields.name = "kook-pixiv-chan";
 bot.logger.addStream({ level: bot.logger.INFO, stream: process.stdout });
@@ -51,7 +54,26 @@ bot.messageSource.on('message', (e) => {
 });
 bot.addCommands(pixivMenu);
 bot.addCommands(pixivAdminMenu);
+
+/**
+ * Add Chinese alias
+ */
+bot.addAlias(pixivMenu, "p站", "P站");
+pixivMenu.addAlias(top, "热门");
+pixivMenu.addAlias(random, "随机");
+pixivMenu.addAlias(detail, "插画");
+pixivMenu.addAlias(author, "作者");
+bot.addAlias(top, "p站热门", "P站热门", "pixiv热门");
+bot.addAlias(random, "p站随机", "P站随机", "pixiv随机");
+bot.addAlias(detail, "p站插画", "P站插画", "pixiv插画");
+bot.addAlias(author, "p站作者", "P站作者", "pixiv作者", "pixiv画师");
+
+/**
+ * Add quick hand alias for .pixiv random and .pixiv top
+ */
 bot.addAlias(random, "色图", "涩图", "setu", "瑟图", "蛇图")
+bot.addAlias(top, "不色图", "不涩图", "busetu", "不瑟图", "不蛇图")
+
 bot.connect();
 bot.logger.debug('system init success');
 
