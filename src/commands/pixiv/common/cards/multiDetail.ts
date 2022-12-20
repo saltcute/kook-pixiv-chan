67c1b606e9df7f4f6d2efe9e8e955283f877b807
Hexona <xuanjiap@lolicon.ac.cn>
2022-12-20 13:08:31 -0500

    fix: catch card generation error

--- src/commands/pixiv/common/cards/multiDetail.ts
+++ src/commands/pixiv/common/cards/multiDetail.ts
@@ -1,4 +1,5 @@
 import { Card, CardObject } from "kbotify"
+import error from "./error";
 
 class MultiDetailCard extends Card {
     constructor(content?: string | CardObject) {
@@ -78,52 +79,56 @@ class MultiDetailCard extends Card {
 
 export default (data: any, curLink: string, idx: number, pid: string[], link: string[], type: "tag" | "top" | "random" | "author", inheritData?: any) => {
     // console.log(pid, idx, pid[idx - 1], pid[idx], pid[idx + 1]);
-    return new MultiDetailCard()
-        .setTheme("info")
-        .setSize("lg")
-        .addText(`** ${(() => {
-            if (data.x_restrict == 0) return data.title;
-            else return `不可以涩涩`;
-        })()}** `)
-        .addModule({
-            "type": "context",
-            "elements": [
-                {
-                    "type": "kmarkdown",
-                    "content": `** [${data.user.name}](https://www.pixiv.net/users/${data.user.uid})**(${data.user.uid}) | [pid ${data.id}](https://www.pixiv.net/artworks/${data.id})`
-                }
-            ]
-        },)
-        .addDivider()
-        .addControl(idx, pid, link, type, inheritData)
-        .addImage(curLink)
-        .addModule({
-            "type": "context",
-            "elements": [
-                {
-                    "type": "kmarkdown",
-                    "content": "喜欢 Pixiv酱吗？来 [Bot Market](https://www.botmarket.cn/bots?id=8) 留下一个五星好评吧！\n您也可以在[爱发电](https://afdian.net/@hexona)帮助Pixiv酱的开发！\n[问题反馈&建议](https://kook.top/iOOsLu)"
-                }
-            ]
-        })
-        .addDivider()
-        .addModule({
-            "type": "context",
-            "elements": [
-                {
-                    "type": "kmarkdown",
-                    "content": `${((): string => {
-                        if (data.x_restrict == 0) {
-                            var str = ""
-                            for (const val of data.tags) {
-                                str += `[#${val.name}](https://www.pixiv.net/tags/${val.name.replace(")", "\\)")}/illustrations)${val.translated_name == null ? "" : ` ${val.translated_name}`} `
+    try {
+        return new MultiDetailCard()
+            .setTheme("info")
+            .setSize("lg")
+            .addText(`** ${(() => {
+                if (data.x_restrict == 0) return data.title;
+                else return `不可以涩涩`;
+            })()}** `)
+            .addModule({
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "kmarkdown",
+                        "content": `** [${data.user.name}](https://www.pixiv.net/users/${data.user.uid})**(${data.user.uid}) | [pid ${data.id}](https://www.pixiv.net/artworks/${data.id})`
+                    }
+                ]
+            },)
+            .addDivider()
+            .addControl(idx, pid, link, type, inheritData)
+            .addImage(curLink)
+            .addModule({
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "kmarkdown",
+                        "content": "喜欢 Pixiv酱吗？来 [Bot Market](https://www.botmarket.cn/bots?id=8) 留下一个五星好评吧！\n您也可以在[爱发电](https://afdian.net/@hexona)帮助Pixiv酱的开发！\n[问题反馈&建议](https://kook.top/iOOsLu)"
+                    }
+                ]
+            })
+            .addDivider()
+            .addModule({
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "kmarkdown",
+                        "content": `${((): string => {
+                            if (data.x_restrict == 0) {
+                                var str = ""
+                                for (const val of data.tags) {
+                                    str += `[#${val.name}](https://www.pixiv.net/tags/${val.name.replace(")", "\\)")}/illustrations)${val.translated_name == null ? "" : ` ${val.translated_name}`} `
+                                }
+                                return str;
+                            } else {
+                                return "#不可以涩涩";
                             }
-                            return str;
-                        } else {
-                            return "#不可以涩涩";
-                        }
-                    })()}`
-                }
-            ]
-        });
+                        })()}`
+                    }
+                ]
+            });
+    } catch (err) {
+        return error(err);
+    }
 }
