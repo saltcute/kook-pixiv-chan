dc3352c28a9f8ee89c75a1c2e92067c787781fdd
Hexona <xuanjiap@lolicon.ac.cn>
2022-12-29 22:50:31 -0500

    feat: colorize reachesLimit

--- src/commands/pixiv/common/cards/reachesLimit.ts
+++ src/commands/pixiv/common/cards/reachesLimit.ts
@@ -7,7 +7,15 @@ export default (user: users.user, type: "command" | "illust", trigger: users.com
         .setSize("lg")
         .addTitle("达到限制 | Reaches Limit")
         .addDivider()
-        .addText(`您已达到当前级别（${user.pixiv.tier}）的[使用限制](${users.tiersListImageLink})。\n（${type == "command" ? `${users.tiersCommandLimit(user, trigger)} 次 \`.pixiv ${trigger}\` 请求` : `${users.tiersIllustLimit(user)} 张插画`}）`)
-        .addText(`如需继续使用，您可以：\n　　北京时间每天04:00自动刷新\n　　购买[更高等级订阅](${users.afdianTierLink[users.getHigherTier(user.pixiv.tier)]})\n　　购买[Quantum组合包](${users.afdianTierLink.Quantum})\n更多信息或问题咨询请至[Pixiv酱官方服务器](https://kook.top/iOOsLu)。`)
-        .addText("发送`.pixiv profile`查看详细用量。");
+        .addText(`您已达到当前级别 (font)${user.pixiv.tier}(font)[primary] 的[使用限制](${users.tiersListImageLink})。\n(font)${type == "command" ? `${users.tiersCommandLimit(user, trigger)} 次 \`.pixiv ${trigger}\` 请求` : `${users.tiersIllustLimit(user)} 张插画`}(font)[danger]`)
+        .addText(`如需继续使用，您可以：\n　　北京时间每天 (font)12:00(font)[pink] 自动刷新 (font)04:00 UTC(font)[tips]\n　　购买[更高等级订阅](${users.afdianTierLink[users.getHigherTier(user.pixiv.tier)]})\n　　购买[Quantum组合包](${users.afdianTierLink.Quantum})\n更多信息或问题咨询请至[Pixiv酱官方服务器](https://kook.top/iOOsLu)。`)
+        .addModule({
+            "type": "context",
+            "elements": [
+                {
+                    "type": "kmarkdown",
+                    "content": "发送`.pixiv profile`查看详细用量。"
+                }
+            ]
+        })
 }
