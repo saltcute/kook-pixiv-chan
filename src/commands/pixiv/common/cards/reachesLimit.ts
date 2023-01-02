bf0717062227744d59572771ab72e18226fec635
Hexona <xuanjiap@lolicon.ac.cn>
2023-01-01 21:18:43 -0500

    fix: reachesLimit description

--- src/commands/pixiv/common/cards/reachesLimit.ts
+++ src/commands/pixiv/common/cards/reachesLimit.ts
@@ -9,7 +9,7 @@ export default (user: users.user, type: "command" | "illust", trigger: users.com
         .addTitle("达到限制 | Reaches Limit")
         .addDivider()
         .addText(`您已达到当前级别 (font)${user.pixiv.tier}(font)[${cards.getTierColor(user.pixiv.tier)}] 的[使用限制](${users.tiersListImageLink})。\n(font)${type == "command" ? `${users.tiersCommandLimit(user, trigger)} 次 \`.pixiv ${trigger}\` 请求` : `${users.tiersIllustLimit(user)} 张插画`}(font)[danger]`)
-        .addText(`如需继续使用，您可以：\n　　北京时间每天 (font)12:00(font)[pink] 自动刷新 (font)04:00 UTC(font)[tips]\n　　购买[更高等级订阅](${users.afdianTierLink[users.getHigherTier(user.pixiv.tier)]})\n　　购买[Quantum组合包](${users.afdianTierLink.Quantum})\n更多信息或问题咨询请至[Pixiv酱官方服务器](https://kook.top/iOOsLu)。`)
+        .addText(`如需继续使用，您可以：\n　　北京时间每天 (font)04:00(font)[pink] 自动刷新 (font)(05:00 JST/12:00 PST/15:00 EST)(font)[tips]\n　　购买[更高等级订阅](${users.afdianTierLink[users.getHigherTier(user.pixiv.tier)]})\n　　购买[Quantum组合包](${users.afdianTierLink.Quantum})\n更多信息或问题咨询请至[Pixiv酱官方服务器](https://kook.top/iOOsLu)。`)
         .addModule({
             "type": "context",
             "elements": [
