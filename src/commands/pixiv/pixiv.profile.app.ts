841e9a42ebafe9786376fb9aefa207fc1053b75e
Hexona <xuanjiap@lolicon.ac.cn>
2023-03-06 11:01:20 -0500

    feat: support looking up profile by user ID for admins

--- src/commands/pixiv/pixiv.profile.app.ts
+++ src/commands/pixiv/pixiv.profile.app.ts
@@ -1,6 +1,7 @@
 import { bot } from 'init/client';
 import { BaseCommand, BaseSession, CommandFunction } from "kasumi.js";
 import * as pixiv from './common'
+import * as pixivadmin from './admin/common'
 
 class Profile extends BaseCommand {
     name = 'profile';
@@ -9,12 +10,24 @@ class Profile extends BaseCommand {
         if (pixiv.common.isBanned(session, this.name)) return;
         if (pixiv.common.isRateLimited(session, 5, this.name)) return;
         pixiv.common.logInvoke(`.pixiv ${this.name}`, session);
-        pixiv.users.detail({
-            id: session.author.id,
-            identifyNum: session.author.identify_num,
-            username: session.author.username,
-            avatar: session.author.avatar
-        }).then((res) => {
+        let userId, data: pixiv.users.userMeta;
+        if (pixivadmin.common.isAdmin(session.authorId) && (userId = session.args[0])) {
+            let user = await bot.API.user.view(userId);
+            data = {
+                id: user.id,
+                identifyNum: user.identify_num,
+                username: user.username,
+                avatar: user.avatar
+            }
+        } else {
+            data = {
+                id: session.author.id,
+                identifyNum: session.author.identify_num,
+                username: session.author.username,
+                avatar: session.author.avatar
+            }
+        }
+        pixiv.users.detail(data).then((res) => {
             return session.send([pixiv.cards.profile(res)]);
         }).catch((e) => {
             this.logger.warn(e);
